b6c4cd7114b34648e06f47188b54acadeed37290
Taiju Suzuki <k39suzuki@gmail.com>
2026-04-14 17:23:15 +0900

    Reorganize into src/, examples/, tests/ directory structure

---
+++ src/__init__.py
@@ -0,0 +1,12 @@
+from .invariant_forms import find_ad_invariant_form, print_result, result_to_dict, BilinearFormResult
+from .lie_algebras import get_algebra, list_algebras, BUILTIN_ALGEBRAS
+
+__all__ = [
+    "find_ad_invariant_form",
+    "print_result",
+    "result_to_dict",
+    "BilinearFormResult",
+    "get_algebra",
+    "list_algebras",
+    "BUILTIN_ALGEBRAS",
+]
